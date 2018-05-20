a61c618daa75d778d2532f853abcf77aea32649c
Kevin Chow <kevinchow@Kevins-MacBook-Pro.local>
2018-05-20 16:55:41 -0700

    resolve_error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bs4
+from nltk.tokenize import RegexpTokenizer
 import re
 import os
 import json
@@ -29,7 +30,7 @@ class Database():
 
     def update_index(self):
         file = open(self.index_path, 'w')
-        json.dump(self.index, file, sort_keys=True, indent=4)
+        file.write(IndexFormatter(self.index).dumps())
         file.close()
 
     def add_token(self, token:str):
@@ -48,27 +49,6 @@ class Database():
             self.add_file(token, file)
         self.index[token][file]['frequency'] = frequency
 
-    # KEVIN ADDED FUNCTION
-    def add_occurences_temp(self, token:str, file:str, indices_info:list):
-        # iterating through the list of lines
-        line_count = 0
-        offset_count = 0
-        for line in indices_info:
-            line_count += 1
-            line = line.lower()
-            if token in line:
-                offset_count = 0
-                for word in line.split().strip('\n'):
-                    offset_count += 1
-                    if token == word:
-                        break
-        if token not in self.index:
-            self.add_token(token)
-        if file not in self.index[token]:
-            self.add_file(token, file)
-        self.index[token][file]['position'] = (line_count, offset_count)
-
-
     def add_length(self, token:str, file:str, length:int):
         if token not in self.index:
             self.add_token(token)
@@ -83,10 +63,38 @@ class Database():
             self.add_file(token, file)
         self.index[token][file][scoreField] = score
 
-    def add_all_files_from_text(self, file:str, text_set:{str}):
-        print(file, text_set)
-        for token in text_set:
-            self.add_file(token, file)
+#Code that Formats the Index into a String Form
+class IndexFormatter():
+    def __init__(self, object: dict):
+        self.object = object
+
+    def dumps(self):
+        result = "{\n"
+        token_dict = sorted(self.object)
+        for token in token_dict:
+            result += '\t"' + token + '": {\n'
+            path_dict = sorted(self.object[token], key=webpage_paths_sorting_key)
+            for path in path_dict:
+                result += '\t\t"' + path + '": { '
+                attribute_dict = sorted(self.object[token][path])
+                for attribute in attribute_dict:
+                    result += '"' + attribute + '": ' + str(self.object[token][path][attribute])
+                    if attribute != attribute_dict[-1]: result += ', '
+                if path != path_dict[-1]: result += ' },\n'
+                else: result += ' }\n'
+            if token != token_dict[-1]: result += '\t},\n'
+            else: result += '\t}\n'
+        result += "}"
+        return result
+
+class IndexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        print('indexEncoder !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        if isinstance(obj, dict):
+            print('passed through!')
+            return json.dumps('')
+        else:
+            return json.dumps(obj)
 
 
 class Parser():
@@ -94,32 +102,35 @@ class Parser():
     def __init__(self, html:str):
         self.soup = bs4.BeautifulSoup(html, 'lxml') #lxml, html.parser
 
+        #removes tags from the parser
+        for tag in self.soup(['script', 'style']):
+            tag.decompose()
+
     def process_text(self, text:str) -> str:
         items = ['\n', '\t', '|', '.', ',', ':', ';']
         for i in items:
             text = text.replace(i, ' ')
+        text = re.sub(' +', ' ', text)  # removes multiple spaces
         text = self.remove_html_code(text)
-        text = text.lower() #lowercase all text
         return text
 
     # Some of the unstructure html will contain coding segments which
     # need to be removed as much as possible from the text
     def remove_html_code(self, text:str) -> str:
-        text = re.sub('#.*{.*}', ' ', text)  # removes css header
-        text = re.sub('if[ ]?[(][^)]*[)]', ' ', text)  # removes code if ()
-        text = re.sub('[^ ]* = function[(][)] {.*}', ' ', text)  # removes code function = ()
-        text = re.sub('function[ ][^(]*[(][)][ ]?{[^}]*}', ' ', text) # removes code function () { }
-        text = re.sub('var [^ ]* = [^(]*[(][^)]*[)]', ' ', text)  # removes code car = ()
-        text = re.sub('[\\].*[\\]["]', ' ', text) # removes code \  \"
-        text = re.sub('catch[(]e[)][ ]?{[^}]*}', ' ', text) # remvoes code catch(e) {}
-        text = re.sub(' +', ' ', text)  # removes multiple spaces
+        # text = re.sub('#[^{]*{[^}]*}', ' ', text)  # removes css header
+        # text = re.sub('if[ ]?[(][^)]*[)]', ' ', text)  # removes code if ()
+        # text = re.sub('[^ ]* = function[(][)] {.*}', ' ', text)  # removes code function = ()
+        # text = re.sub('function[ ][^(]*[(][)][ ]?{[^}]*}', ' ', text) # removes code function () { }
+        # text = re.sub('var [^ ]* = [^(]*[(][^)]*[)]', ' ', text)  # removes code var = ()
+        # text = re.sub('[\\].*[\\]["]', ' ', text) # removes code \  \"
+        # text = re.sub('catch[(]e[)][ ]?{[^}]*}', ' ', text) # remvoes code catch(e) {}
         return text
 
     def all_title_text(self) -> str:
-        print('---title----')
         result = ""
         for title in self.soup.find_all('title'):
-            result += title.string + ' '
+            if title.string != None:
+                result += title.string + ' '
         return result
 
     def all_header_text(self) -> str:
@@ -134,21 +145,19 @@ class Parser():
         return self.soup.get_text()
 
 class Tokenizer():
-    __slots__ = ('text', 'text_list', 'text_set')
+    __slots__ = ('text', 'text_list', 'text_list_lower', 'text_set', 'text_set_lower')
     def __init__(self, text:str):
         self.text = text
         self.text_list = self.tokenize()
+        self.text_list_lower = self.tokenize_lower()
         self.text_set = set(self.text_list)
-        if "" in self.text_set:
-            self.text_set.remove("")
+        self.text_set_lower = set(self.text_list_lower)
 
     def tokenize(self) -> [str]:
-        text_list = self.text.split(' ')
-        for text in text_list:
-            if not text.isalnum():
-                text_list.remove(text)
-        return text_list
+        return RegexpTokenizer(r'\w+').tokenize(self.text)
 
+    def tokenize_lower(self) -> [str]:
+        return [token.lower() for token in RegexpTokenizer(r'\w+').tokenize(self.text)]
 
 def token_frequency_in_document(token:str, text_list: [str]) -> int:
     frequency = 0
@@ -182,17 +191,9 @@ def webpage_paths_sorting_key(s: str):
     file = s.split('/')
     return (int(file[1]), int(file[2]))
 
-def is_text_html_structured(text:str) -> bool:
-    html_tags = ["html", "body", "link", "title"]
-    lower_text = text.lower()
-    for tag in html_tags:
-        if tag in lower_text:
-            return True
-    return False
-
 def pages_to_ignore() -> set:
     result = set()
-    ignore = ['0/0','39/373']
+    ignore = ['0/0', '0/438', '39/373']
     for page in ignore:
         page = "WEBPAGES_RAW/" + page
         result.add(page)
@@ -206,7 +207,7 @@ def main():
     webpage_paths = all_webpage_paths()
     webpage_paths = webpage_paths[:400] #EDIT HOW MANY PATHS WANTED/COMMENT OUT IF RUNNING ALL FILES
     TOTAL_DOCUMENTS = len(webpage_paths)
-    for path in webpage_paths:
+    for path in webpage_paths: #6:15
 
         if path in pages_to_ignore():
             continue
@@ -231,6 +232,24 @@ def main():
             index.add_frequency(token, path, frequency)
             index.add_length(token, path, len(tokenizer.text_list))
 
+        title_text = parser.process_text(parser.all_title_text())
+
+        # print(parser.all_text())
+
+        tokenizer = Tokenizer(webpage_text)
+        title_tokenizer = Tokenizer(title_text)
+
+        for token in tokenizer.text_set_lower:
+            index.add_file(token, path)
+            text_frequency = token_frequency_in_document(token, tokenizer.text_list_lower)
+            title_frequency = token_frequency_in_document(token, title_tokenizer.text_list_lower)
+
+            index.add_frequency(token, path, text_frequency)
+            index.add_title_frequency(token, path, title_frequency)
+
+            index.add_length(token, path, len(tokenizer.text_list_lower))
+            index.add_title_length(token, path, len(title_tokenizer.text_list_lower))
+
     start = time()
     for token in index.index:
         for file in index.index[token]:
@@ -242,6 +261,7 @@ def main():
 
     index.update_index()
     print('total documents:', TOTAL_DOCUMENTS)
+    print(len(index.index))
 
 
 #Use this to MANUALLY CLEAR INDEX.JSON when experimenting
