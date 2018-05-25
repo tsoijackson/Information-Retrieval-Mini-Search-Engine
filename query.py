af0a26b0ced076a7d4b7522e6faebebb5534ecc2
Stanley <stanlekt@uci.edu>
2018-05-25 05:14:22 -0700

    Merge branch 'master' of https://github.com/tsoijackson/Information-Retrieval-Mini-Search-Engine
    added query

--- query.py
+++ query.py
@@ -33,17 +33,62 @@ def test():
 							print("---------------------------------")
 				
 
+with open("index.json", 'r') as f:
+	google_dict = json.load(f)
+
+with open("WEBPAGES_RAW/bookkeeping.json", 'r') as f:
+	url_dict = json.load(f)
+
+user_input = input("Please enter a search: ")
+
 # Separate the query and normalize (lowercase all) it
 def processQuery(query:str) -> [str]:
-	queryList = []
+        queryList = []
+        split_query = query.split(" ")
+        for word in split_query:
+                queryList.append(word.lower())
+        return queryList
 
-	return queryList
+### returns highest tf-idf score of all the query words
+def highest_scores(index: dict, split_words):
+        scores = []
+        for word in index:
+                for user_word in split_words:
+                        if user_word == word:
+                                for terms in index[word].values():
+                                        scores.append(terms["tf-idf"])
+        scores = [float(x) for x in scores]
+        scores.sort(key = int, reverse = True)
+        return scores[0:10]
 
-
+        
 # Takes in query and returns the top 10 links
 def searchQuery(query:str, index:dict, bookkeeping:dict, maxlinks:int) -> [str]:
-	result = []
-	processQuery(query)
+        result = []
+        split_words = processQuery(query)
+        high_scores = highest_scores(index, split_words)
+        files = []
+        for word in index:
+                for file in index[word]:
+                        for terms in index[word].values():
+                                if maxlinks != 0:
+                                        for score in high_scores:
+                                                for user_word in split_words:
+                                                        if user_word == word:
+                                                                if terms["tf-idf"] == score:
+                                                                        if file not in files:
+                                                                                files.append(file)
+                                                                                maxlinks -= 1
+        for url in bookkeeping:
+                for file in files:
+                        if file[13:] == url:
+                                if bookkeeping[url] not in result:
+                                        result.append(bookkeeping[url])
+                                                                                
+                        
+                                
+                                        
+        print(len(result))                                               
+        return result
 
-
-	return result
+print(searchQuery(user_input, google_dict, url_dict, 10))
