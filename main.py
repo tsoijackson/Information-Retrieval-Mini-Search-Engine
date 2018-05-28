53898ad3937a5eb7a00ca7a10413d3256c459186
Kevin Chow <kevinchow@Kevins-MacBook-Pro.local>
2018-05-27 18:50:05 -0700

    added ec func

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 
 WEBPAGES_PATH = "WEBPAGES_RAW"
 BOOK_KEEPING_PATH = "WEBPAGES_RAW/bookkeeping.json"
-INDEX_PATH = "index.json"
+INDEX_PATH = "mysite/home/main/index.json"
 
 # class that defines the json "db" and its functions
 class Database():
@@ -97,16 +97,6 @@ class Database():
         if file not in self.index[token]:
             self.add_file(token, file)
         self.index[token][file]['position'] = [line_count, offset_count]
-
-    def add_additionalweights(self, token:str, file:str, indices_text:list):
-        if token not in self.index:
-            self.add_token(token)
-        if file not in self.index[token]:
-            self.add_file(token, file)
-        self.index[token][file]['positional_weight'] = 0
-        self.index[token][file]['numterm_weight'] = 0
-        self.index[token][file]['header_weight'] = 0
-
 
 #Code that Formats the Index into a String Form
 class IndexFormatter():
@@ -217,38 +207,146 @@ def documents_containing_token(token:str, database: Database.index) -> int:
 def idf(total_documents:int, documents_containing_token:int) -> float:
     return math.log(total_documents / (1 + documents_containing_token))
 
-def tfidf(token_frequency:int, total_docments:int, documents_containing_token:int) -> float:
-    return tf(token_frequency) * idf(total_docments,documents_containing_token)
+def tfidf(token_frequency:int, total_documents:int, documents_containing_token:int) -> float:
+    return tf(token_frequency) * idf(total_documents,documents_containing_token)
 
+# EC: calculate the average tf-idf of a term
+def qterm_avg(current_term,google_dict):
+    print("------------- avg function is called -------------")
+    current_avg = 0
+    for doc, values in google_dict[current_term].items():
+        current_avg += values['tf-idf']
+        print(current_avg)
+    return current_avg/len(google_dict[current_term])
+
+# EC: extract the docs related with
+def qterm_docextract1(query_list, google_dict):
+    # list of all the document that contains one or more of the query term
+    document_dic = dict([])
+    for term in query_list:
+        for doc in google_dict[term]:
+            if doc not in document_dic:
+                document_dic[doc] = []
+                document_dic[doc].append([term,google_dict[term][doc]["position"]])
+
+            else:
+                # lists of list that contains the term and the position
+                document_dic[doc].append([term,google_dict[term][doc]["position"]])
+    return document_dic
+def qterm_docextract2(query_list, google_dict):
+    # list of all the document that contains one or more of the query term
+    document_dic = dict([])
+    for term in query_list:
+        for doc in google_dict[term]:
+            if doc not in document_dic:
+                document_dic[doc] = []
+                document_dic[doc].append(term)
+            else:
+                document_dic[doc].append(term)
+    return document_dic
+# EC: check to see the type and if a query term is within a header within
+# the documentation
+def qterm_withindocument(query_term, current_doc):
+    ans_list = []
+    with open(current_doc) as current_file:
+        current_soup = bs4.BeautifulSoup(current_file, "lxml")
+    possible_tags = ["title", "h1", "b", "i"]
+    for tag in possible_tags:
+        tag_list = current_soup.select(tag)
+        for individual_tag in tag_list:
+            # print(individual_tag)
+            if query_term in str(individual_tag):
+                ans_list.append(tag)
+    return ans_list
+
+def tfidf_reset(query_term, google_dict):
+    for query in query_term:
+        for docs in google_dict[query]:
+            google_dict[query][docs]["tf-idf"] = google_dict[query][docs]["tf-idf-temp"]
 
 # EC: calculate and manipulate the weight based on a tokens positional location
 def qterm_positional_weight(query_list,google_dict):
+    print("------ qterm_positional_ec ------")
+    if(len(query_list) < 2):
+        return None
+    # list of all the document that contains one or more of the query term
+    document_dic = qterm_docextract1(query_list,google_dict)
+    line_result = 0
+    column_result = 0
+    prev_item = None
+    # go through each term within the query to calculate the line and row result
+    # key: document, value: [ [term, position], ... ]
+    # we are moving through each document
+    for key, values in document_dic.items():
+        # this list will collect the terms that has been scanned through
+        current_terms = set()
+        # moves through one document at a time
+        if len(values) == 1:
+            return None
+        for queries_info in values:
+            if prev_item != None:
+                # moving through each query term, its position within the document
+                line_result += math.fabs(queries_info[1][0]-prev_item[1][0])
+                column_result += math.fabs(queries_info[1][1]-prev_item[1][1])
+                current_terms.add(queries_info[0])
+            prev_item = queries_info
+        # update the positional_weight and modify the tf_idf of the document
+        # with a term key of an inverse index
+        if (line_result > 0 and column_result > 0):
+            # the smaller the value the more accurate that it is; therefore we need
+            # to use the inverse
+            line_result = 1/line_result
+            column_result = 1/column_result
+            for term in current_terms:
+                print(term)
+                print(key)
+                print("prior: ", google_dict[term][key]['tf-idf'])
+                # print("current avg: ", current_avg)
+                # print("line_result", line_result)
+                # print("column_result", column_result)
+                updated_score = 1 + math.log((2*line_result + column_result) * 10) # use of 10 as the base tf-idf val
+                print("updated score", updated_score)
+                # temporarily placed into the tf-idf-temp section
+                google_dict[term][key]['tf-idf-temp'] = google_dict[term][key]['tf-idf']
+                google_dict[term][key]['tf-idf'] += updated_score
+                print("later: ", google_dict[term][key]['tf-idf'])
+
+# EC: calculate the weight based on the type of header it is contained within
+def qterm_header_weight(query_list,google_dict):
+    print("------ qterm_header_ec ------")
     if(len(query_list) == 0):
         return None
-    document_dic = dict(list)
-    for term in query_list:
-        for doc in google_dict[term]:
-            document_dic[doc].append([term,google_dict[term].position])
+    # list of all the document that contains one or more of the query term
+    document_dic = qterm_docextract2(query_list,google_dict)
+    header_weight = 0
+    # key: document, value: [term, ...]
     for key, values in document_dic.items():
-        if value > 1:
-            multiplier = 0
-            # same line receives 3x the score compared to the width
-
-
-
-
-
-
-
-
-
-
-
-
-# EC: calculate and manipulate the weight based on the type of header it is contained within
-def qterm_header_weight(query_list):
-
-
+        # print(key, values)
+        header_weight = 0
+        for term in values:
+            # this is the output of all the tags that contains a query term
+            # within a document
+            qterm_list = qterm_withindocument(term, key)
+            # print("qterm_list", qterm_list)
+            if ("title" in qterm_list):
+                header_weight += 10
+            elif ("h1" in qterm_list):
+                header_weight += 5
+            elif ("b" in qterm_list or "i" in qterm_list):
+                header_weight += 3
+        # update the tf_idf value of this particular document within each
+        # of the qterm in the inverted index
+        if (header_weight > 0):
+            for term in values:
+                print("prior: ", google_dict[term][key]['tf-idf'])
+                current_term_frequency = google_dict[term][key]['frequency']
+                updated_term_frequency = current_term_frequency + header_weight
+                idf_value = google_dict[term][key]['idf']
+                tf_idf_update = tf(updated_term_frequency) * idf_value
+                # temporarily placed into the tf-idf-temp section
+                google_dict[term][key]['tf-idf-temp'] = google_dict[term][key]['tf-idf']
+                google_dict[term][key]['tf-idf'] = tf_idf_update
+                print("later: ", google_dict[term][key]['tf-idf'])
 
 def all_webpage_paths() -> [str]:
     result = []
@@ -277,9 +375,9 @@ def main():
     index = Database(BOOK_KEEPING_PATH, INDEX_PATH) #initialize inversed index
 
     webpage_paths = all_webpage_paths()
-    webpage_paths = webpage_paths[0:400] #EDIT HOW MANY PATHS WANTED/COMMENT OUT IF RUNNING ALL FILES
+    webpage_paths = webpage_paths[0:500] #EDIT HOW MANY PATHS WANTED/COMMENT OUT IF RUNNING ALL FILES
     TOTAL_DOCUMENTS = len(webpage_paths)
-    for path in webpage_paths: #6:15
+    for path in webpage_paths:
 
         if path in pages_to_ignore():
             continue
@@ -317,16 +415,21 @@ def main():
             index.add_length(token, path, len(tokenizer.text_list_lower))
             index.add_title_length(token, path, len(title_tokenizer.text_list_lower))
             index.add_occurences(token, path, indices_info)
+
         title_text = parser.process_text(parser.all_title_text())
 
         # print(parser.all_text())
     start = time()
     for token in index.index:
         for file in index.index[token]:
-            tf_idf = tfidf(index.index[token][file]['frequency'],
-                           total_docments=len(webpage_paths),
+            current_idf = idf(total_documents=len(webpage_paths),
+            documents_containing_token=documents_containing_token(token, index.index))
+            current_tf_idf = tfidf(index.index[token][file]['frequency'],
+                           total_documents=len(webpage_paths),
                            documents_containing_token=documents_containing_token(token, index.index))
-            index.add_score(token, file, 'tf-idf', tf_idf)
+            index.add_score(token, file, 'tf-idf', current_tf_idf)
+            index.add_score(token, file, 'tf-idf-temp', 0)
+            index.add_score(token, file, 'idf', current_idf)
     print('adding score :', time() - start)
 
     index.update_index()
